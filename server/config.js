35012c544593d4b34cef046d8d2ac895b66d5c47
Christina Chu <cchu@Christinas-Air.attlocal.net>
2025-07-22 19:12:38 -0700

    config

--- server/config.js
+++ server/config.js
@@ -1,25 +1,25 @@
-const { config } = require('dotenv');
+// const { config } = require('dotenv');
 
-const logger = require('./logger');
+// const logger = require('./logger');
 
-// package.json sets NODE_ENV in its scripts
+// // package.json sets NODE_ENV in its scripts
 const isProduction = process.env.NODE_ENV === 'production';
 
-// load configuration based on environment
-const { error, parsed } = config({
-  path: isProduction ? '.env.production' : '.env.sandbox',
-});
+// // load configuration based on environment
+// const { error, parsed } = config({
+//   path: isProduction ? '.env.production' : '.env.sandbox',
+// });
 
-if (error) {
-  // likely file missing
-  logger.error(`Error loading configuration: ${error}`);
-}
+// if (error) {
+//   // likely file missing
+//   logger.error(`Error loading configuration: ${error}`);
+// }
 
-// PROTIP: get more insight by running in debug mode: `DEBUG=* npm run dev`
-logger.debug('Parsed configuration:', parsed);
+// // PROTIP: get more insight by running in debug mode: `DEBUG=* npm run dev`
+// logger.debug('Parsed configuration:', parsed);
 
-// export secrets stored in .env.production or .env.sandbox (based on .env.example)
+// // export secrets stored in .env.production or .env.sandbox (based on .env.example)
 module.exports = {
-  ...parsed,
+  accessToken: process.env.SQUARE_ACCESS_TOKEN,
   isProduction,
 };
